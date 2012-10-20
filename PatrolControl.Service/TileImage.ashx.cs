5606032a7bd2b7fc1eea4e63426584cf81fb3f4e
Pavel Krymets <pavel@krymets.com>
2012-10-20 10:19:33 +0300

    Add domain service and map tile handlers

---
+++ PatrolControl.Service/TileImage.ashx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace PatrolControl.Service
+{
+    /// <summary>
+    /// Summary description for TileImage
+    /// </summary>
+    public class TileImage : IHttpHandler
+    {
+
+        private string _tilePath = ConfigurationManager.AppSettings["TilePath"];
+
+        public void ProcessRequest(HttpContext context)
+        {
+            var x = context.Request.Params["x"];
+            var y = context.Request.Params["y"];
+            var z = context.Request.Params["z"];
+            if (x == null || y == null || z == null || _tilePath == null)
+            {
+                context.Response.Write("X or Y or Z parameters or TilePath in web.config is not set");
+                return;
+            }
+
+            context.Response.WriteFile(string.Format(_tilePath, x, y, z));
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
