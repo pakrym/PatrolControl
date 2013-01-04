01cadfeb5e94417d9f4cea1e5f3bad91b13970fe
Pavel Krymets <pavel@krymets.com>
2013-01-04 08:24:19 +0200

    work on UserManager

--- PatrolControl.UI/Screens/Common/Map/BuildingFeatureLayerViewModel.cs
+++ PatrolControl.UI/Screens/Common/Map/BuildingFeatureLayerViewModel.cs
@@ -16,19 +16,4 @@ namespace PatrolControl.UI.Screens.Common.Map
             client.GetBuildingsAsync();
         }
     }
-
-    public class StreetFeatureLayerViewModel : FeatureLayerViewModel
-    {
-        public override string Name
-        {
-            get { return "Streets"; }
-        }
-
-        public override void Update(ESRI.ArcGIS.Client.Geometry.Envelope envelope)
-        {
-            var client = new PatrolControlServiceClient();
-            client.GetStreetsCompleted += (sender, args) => SetFeatures(args.Result);
-            client.GetStreetsAsync();
-        }
-    }
 }
