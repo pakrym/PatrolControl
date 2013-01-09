73c6f2c7954c35ad867210729f60184c0c55f80f
Andrey Tretyak <andrey.o.tretyak@gmail.com>
2013-01-09 12:19:00 +0200

    Password fix

--- PatrolControl.Service/Model/Sql/0100-InsertUsers.sql
+++ PatrolControl.Service/Model/Sql/0100-InsertUsers.sql
@@ -1,5 +1,5 @@
 ﻿INSERT INTO Users (Name, PasswordHash, Type)
 VALUES 
-('MapEditor', '1', 1),
-('UserManager', '2', 2),
-('Superuser', '2', 3)
+('MapEditor',   'c4ca4238a0b923820dcc509a6f75849b', 1),
+('UserManager', 'c81e728d9d4c2f636f067f89cc14862c', 2),
+('Superuser',   'eccbc87e4b5ce2fe28308fd9f2a7baf3', 3)
