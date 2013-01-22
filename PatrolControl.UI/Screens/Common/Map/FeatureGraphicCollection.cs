96321bf079af647634eddf56d17c21693b91317c
Pavel Krymets <pavel@krymets.com>
2013-01-22 17:56:08 +0200

    add new files

---
+++ PatrolControl.UI/Screens/Common/Map/FeatureGraphicCollection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using ESRI.ArcGIS.Client;
+using PatrolControl.UI.PatrolControlServiceReference;
+
+namespace PatrolControl.UI.Screens.Common.Map
+{
+    public class FeatureGraphicCollection : GraphicCollection
+    {
+        private readonly ObservableCollection<Entity> _features;
+
+        public FeatureGraphicCollection(ObservableCollection<Entity> features)
+        {
+            _features = features;
+            _features.CollectionChanged += FeaturesChanged;
+        }
+
+        private void FeaturesChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            //TODO: Optimize me
+            this.Clear();
+            foreach (Feature feature in _features)
+            {
+                this.Add(ConvertFeature(feature));
+            }
+        }
+
+        private FeatureGraphic ConvertFeature(Feature feature)
+        {
+            return new FeatureGraphic(feature);
+        }
+    }
+}
---
+++ PatrolControl.UI/Service References/Entity.cs
@@ -0,0 +1,10 @@
+﻿using ESRI.ArcGIS.Client;
+using PatrolControl.UI.PatrolControlServiceReference;
+
+namespace PatrolControl.UI.PatrolControlServiceReference
+{
+    public partial class Entity
+    {
+        internal EntityState State { get; set; }
+    }
+}
