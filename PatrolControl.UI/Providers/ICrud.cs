1b55dfa4eceba2d45c22cc3f1819912bfb801f8e
Pavel Krymets <pavel@krymets.com>
2013-01-31 14:21:38 +0200

    righ editor

---
+++ PatrolControl.UI/Providers/ICrud.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using PatrolControl.UI.PatrolControlServiceReference;
+
+namespace PatrolControl.UI.Providers
+{
+    public interface ICrud
+    {
+        Task<Entity[]> List();
+
+        Entity New();
+        Task Save(Entity[] entities);
+        Task Add(Entity[] entities);
+        Task Remove(Entity[] entities);
+    }
+}
---
+++ PatrolControl.UI/Screens/Common/Editors/FeatureGraphicEditorViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using Caliburn.Micro;
+using PatrolControl.UI.PatrolControlServiceReference;
+
+namespace PatrolControl.UI.Screens.Common.Editors
+{
+    public class FeatureGraphicEditorViewModel: PropertyChangedBase, IEditableObject
+    {
+        private readonly Feature _feature;
+
+        public FeatureGraphicEditorViewModel(Feature feature)
+        {
+            _feature = feature;
+        }
+        
+        public DbGeography Geography { get; set; }
+
+        public virtual void BeginEdit()
+        {
+            Geography = _feature.Geography;
+        }
+
+        public virtual void EndEdit()
+        {
+            _feature.Geography = Geography;
+        }
+
+        public virtual void CancelEdit()
+        {
+            BeginEdit();
+        }
+    }
+}
