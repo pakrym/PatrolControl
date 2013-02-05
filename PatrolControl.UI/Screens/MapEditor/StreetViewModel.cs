d7ce8a68fa914c2abf4d6666c93121f98f9486b3
Pavel Krymets <pavel@krymets.com>
2013-02-05 14:53:17 +0200

    move item viewmodels

---
+++ PatrolControl.UI/Screens/MapEditor/StreetViewModel.cs
@@ -0,0 +1,14 @@
+﻿using PatrolControl.UI.PatrolControlServiceReference;
+using PatrolControl.UI.Screens.Common.Map;
+using PatrolControl.UI.Utilities;
+
+namespace PatrolControl.UI.Screens.Common.Editors
+{
+    public class StreetViewModel : FeatureViewModel, IEditorViewModel<Street>
+    {
+        public StreetViewModel(Street model)
+            : base(model)
+        {
+        }
+    }
+}
